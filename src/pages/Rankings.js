3ff59f1aff1014c2d208b1d40597ec8271174fce
Jonathan Cady <joncady@uw.edu>
2019-03-17 12:14:25 -0700

    Added ranking information

--- src/pages/Rankings.js
+++ src/pages/Rankings.js
@@ -8,8 +8,11 @@ export default class Rankings extends Component {
             <section id="ranking-page">
                 <h1>Rankings</h1>
                 <p>This page houses all regional Power Rankings for Super Smash Bros. Ultimate in Western Washington. For more on how these rankings are determined, check out our <a href="https://docs.google.com/document/d/1ZnD6aFGwMPAh8dcdpeBx4vEdgYwMQUakqpldTHSnpZg">FAQ.</a></p>
-                <p>The current season runs from 12/7/18 - 3/1/19. Head out to some <Link to="/events">Events</Link> for a shot at making the list!</p>
-                <p>More ranking information coming soon!</p>
+                <p>The current season runs from 3/2/19 - 5/31/19. Head out to some <Link to="/events">Events</Link> for a shot at making the list!</p>
+                <h3>Winter 2018-2019</h3>
+                <div className="rankings-image">
+                    <img style={{ width: '100%' }} src={require('../assets/winpr18.png')} alt="Winter PR 2018-19"></img>
+                </div>
             </section>
         );
     }
